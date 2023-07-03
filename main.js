5292dfb6ebacd85ac4e3954c3babab69d3431dee
havertzz04 <havertzz04@gmail.com>
2023-07-03 17:02:48 -0500

    feat: :zap: Result ny categorie
    Now the user can see his/her status and the BMI

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
 let btn = document.querySelector("#btn");
 
-btn.addEventListener("click", function(){
-    event.preventDefault()
+btn.addEventListener("click", function(event) {
+    event.preventDefault();
 
     let weight = parseInt(document.querySelector("#weight").value);
     let height = parseFloat(document.querySelector("#height").value);
 
     let total = weight / (height ** 2);
 
-    document.querySelector("#textBox").textContent = total.toFixed(1);
-})
+
+    if(total < 18.5){
+        document.querySelector("#textBox");
+        textBox.textContent = `${total.toFixed(1)} (Underweight)`;
+    } else if(total < 24.9){
+        document.querySelector("#textBox");
+        textBox.textContent = `${total.toFixed(1)} (Normal)`;
+    } else if(total < 29.9){
+        document.querySelector("#textBox");
+        textBox.textContent = `${total.toFixed(1)} (Overweight)`;
+    }  else if (total >= 30){
+        document.querySelector("#textBox");
+        textBox.textContent = `${total.toFixed(1)} (Obese)`;
+    } else {
+        document.querySelector("#textBox");
+        textBox.textContent = "Type a valid information.";
+    }
+});

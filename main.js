bfaa2ca44fc1968e1e7f9e0ac9f4e1da2a9efec9
havertzz04 <havertzz04@gmail.com>
2023-07-03 07:25:28 -0500

    feat: :sparkles: Logic
    Now the BMI Calculator have the necessary logic to calculate the BM

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-// Pending
+let btn = document.querySelector("#btn");
+
+btn.addEventListener("click", function(){
+    event.preventDefault()
+
+    let weight = parseInt(document.querySelector("#weight").value);
+    let height = parseFloat(document.querySelector("#height").value);
+
+    let total = weight / (height ** 2);
+
+    document.querySelector("#textBox").textContent = total.toFixed(1);
+})
